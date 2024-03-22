27c8a78308991ffbb0410e38b1c1f5ea574b8a4a
FARIHA MALIK <163237663+farihamalik098@users.noreply.github.com>
2024-03-22 15:16:44 -0700

    Datatypes

---
+++ app.js
@@ -0,0 +1,8 @@
+var username = "fariha";
+console.log(username);
+var age = 17;
+console.log(age);
+var isstudent = true;
+console.log(isstudent);
+var randomvalue = "https ://vt.tiktok.com";
+console.log(randomvalue);
---
+++ app.ts
@@ -0,0 +1,8 @@
+let username : string ="fariha";
+console.log (username);
+let age : number = 17;
+console.log(age);
+let isstudent : boolean = true;
+console.log(isstudent);
+let randomvalue : any = "https ://vt.tiktok.com";
+console.log(randomvalue);
